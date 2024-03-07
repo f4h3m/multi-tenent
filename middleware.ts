dc62b4b70677e70bdc5e3834f8996f246f15d255
WordSphere LLC <info@wordsphere.com>
2024-03-07 23:46:00 +0600

    update middleware.js 10

--- middleware.ts
+++ middleware.ts
@@ -17,6 +17,8 @@ export const config = {
 export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
 
+  console.log("url", url);
+
   // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
   let hostname = req.headers
     .get("host")!
@@ -59,15 +61,15 @@ export default async function middleware(req: NextRequest) {
   // }
 
   // rewrite root application to `/home` folder
-  if (
-    hostname === "localhost:3000" ||
-    hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
-  ) {
-    return NextResponse.rewrite(
-      new URL(`process.env.NEXT_PUBLIC_ROOT_DOMAIN`, req.url)
-    );
-  }
+  // if (
+  //   hostname === "localhost:3000" ||
+  //   hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
+  // ) {
+  //   return NextResponse.rewrite(
+  //     new URL(`process.env.NEXT_PUBLIC_ROOT_DOMAIN`, req.url)
+  //   );
+  // }
 
   // rewrite everything else to `/[domain]/[slug] dynamic route
-  return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
+  // return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
 }
